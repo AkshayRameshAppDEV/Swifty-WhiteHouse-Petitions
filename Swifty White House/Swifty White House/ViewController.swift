e39d9757c0431e463fb229efc59fa39365c93dd4
akshayrameshappdev <akshay_ramesh@yahoo.com>
2020-12-30 17:25:09 -0600

    Added initial starter project

---
+++ Swifty White House/Swifty White House/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swifty White House
+//
+//  Created by Akshay Ramesh on 12/30/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
