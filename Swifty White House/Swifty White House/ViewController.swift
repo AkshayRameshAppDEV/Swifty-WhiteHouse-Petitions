5343568ee7f6206e1b20162a65d8b00f92a630fb
akshayrameshappdev <akshay_ramesh@yahoo.com>
2020-12-30 21:15:06 -0600

    Added top rated tab controller for displaying popular petitions

--- Swifty White House/Swifty White House/ViewController.swift
+++ Swifty White House/Swifty White House/ViewController.swift
@@ -9,15 +9,27 @@ import UIKit
 
 class ViewController: UITableViewController {
     var petitions = [Petition]()
+    var urlString: String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        if navigationController?.tabBarItem.tag == 0 {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        } else {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
+        }
         if let url = URL(string: urlString) {
             if let data = try? Data(contentsOf: url) {
                 parse(json: data)
             }
         }
+        showError()
+    }
+    
+    func showError() {
+        let ac = UIAlertController(title: "Loading Error", message: "The was some server issue", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
     }
     
     func parse(json: Data) {
